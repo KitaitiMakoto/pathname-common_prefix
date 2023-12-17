5c8177817593a3ec5bec42ea77a2ee94fcdf79e9
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2023-12-18 03:06:37 +0900

    Make type annotations more accurate

--- lib/pathname/common_prefix.rb
+++ lib/pathname/common_prefix.rb
@@ -4,20 +4,20 @@ class Pathname
   class << self
     def common_prefix(*paths)
       paths.flatten!
-      return if paths.empty? # @type var paths: Array[Pathname | String]
-      Pathname(paths.pop).common_prefix(paths) # @type var paths: NonEmptyArray[Pathname]
+      return if paths.empty? # @type var paths: Array[Pathname | _ToStr]
+      Pathname(paths.pop).common_prefix(paths) # @type var paths: NonEmptyArray[Pathname | String] Actualy _ToStr instead of String but type of Pathname() requires String
     end
   end
 
   def common_prefix(*others)
     others.flatten!
-    others.map! {|path| Pathname path} # @type var others: Array[Pathname | String]
-    enum_for(:ascend).find {|path|
+    others.map! {|path| Pathname path} # @type var others: Array[Pathname | String] Actualy _ToStr instead of String but type of Pathname() requires String
+    enum_for(:ascend).find {|path| # @type path: Pathname | _ToStr
       others.all?{|other|other.start_with?(path)}
     }
   end
 
   def start_with?(other)
-    enum_for(:descend).include?(Pathname(other))
+    enum_for(:descend).include?(Pathname(other)) # @type var other: Pathname | String Actualy _ToStr instead of String but type of Pathname() requires String
   end
 end
