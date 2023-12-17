7942615ed1f76641cf8013faa030abe68d2aa3b8
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2023-12-18 03:30:08 +0900

    Fix comments for Steep

--- lib/pathname/common_prefix.rb
+++ lib/pathname/common_prefix.rb
@@ -4,20 +4,27 @@ class Pathname
   class << self
     def common_prefix(*paths)
       paths.flatten!
-      return if paths.empty? # @type var paths: Array[Pathname | _ToStr]
-      Pathname(paths.pop).common_prefix(paths) # @type var paths: NonEmptyArray[Pathname | String] Actualy _ToStr instead of String but type of Pathname() requires String
+      return if paths.empty?
+      # @type var paths: NonEmptyArray[Pathname | String]
+      # Actualy _ToStr instead of String but type of Pathname() requires String
+      Pathname(paths.pop).common_prefix(paths)
     end
   end
 
   def common_prefix(*others)
     others.flatten!
-    others.map! {|path| Pathname path} # @type var others: Array[Pathname | String] Actualy _ToStr instead of String but type of Pathname() requires String
-    enum_for(:ascend).find {|path| # @type path: Pathname | _ToStr
+    # @type var others: Array[Pathname | String]
+    # Actualy _ToStr instead of String but type of Pathname() requires String
+    others.map! {|path| Pathname path}
+    enum_for(:ascend).find {|path|
+      # @type path: pathnamable
       others.all?{|other|other.start_with?(path)}
     }
   end
 
   def start_with?(other)
-    enum_for(:descend).include?(Pathname(other)) # @type var other: Pathname | String Actualy _ToStr instead of String but type of Pathname() requires String
+    # @type var other: Pathname | String
+    # Actualy _ToStr instead of String but type of Pathname() requires String
+    enum_for(:descend).include?(Pathname(other))
   end
 end
